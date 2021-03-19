744ac1833ec7d4d02eea0c5558060cfd1b6e6728
ianfelix <ianfelix030@gmail.com>
2021-03-19 09:50:42 -0300

    feat:  created dashboard and summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyles } from './styles/global';
 
 export function App() {
   return (
     <>
-      <GlobalStyles />
       <Header />
+      <Dashboard />
+      <GlobalStyles />
     </>
   );
 }
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export const Summary = () => {
+  return (
+    <S.Container>
+      <h1>Summary</h1>
+    </S.Container>
+  );
+};
