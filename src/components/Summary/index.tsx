47b886837ec95fe0b02429392bee97b6a0fb1734
ianfelix <ianfelix030@gmail.com>
2021-03-19 10:16:49 -0300

    feat: body and styles from summary created

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,34 @@
 import * as S from './styles';
+import IncomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
 
 export const Summary = () => {
   return (
     <S.Container>
-      <h1>Summary</h1>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={IncomeImg} alt='Entradas' />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt='Saídas' />
+        </header>
+        <strong>- R$5000,00</strong>
+      </div>
+
+      <div className='highlight-background'>
+        <header>
+          <p>total</p>
+          <img src={totalImg} alt='Total' />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
     </S.Container>
   );
 };
